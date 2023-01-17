412f2e8b953fc582febbc192c6c3c733f62e32bc
Andy Dunkel <andy.dunkel@ekiwi.de>
2023-01-17 20:09:04 +0100

    Some corrections

--- download.php
+++ download.php
@@ -26,26 +26,28 @@ if (isset($_POST["pass"])) {
 
 $file_name = basename($url);
     
-if (file_put_contents("downloads\\" . $file_name, file_get_contents($url)))
+if (file_put_contents("downloads/" . $file_name, file_get_contents($url)))
 {
-    echo "File downloaded successfully:";
+    echo "File downloaded successfully:<br/>";
 
     $zip = new ZipArchive;
-    $res = $zip->open("downloads\\" . $file_name . ".zip", ZipArchive::CREATE); 
+    $res = $zip->open("downloads/" . $file_name . ".zip", ZipArchive::CREATE);
     
     //Add your file name
     if ($res === TRUE) {
-        $zip->addFile('downloads\\' . $file_name, $file_name);
+        $zip->addFile('downloads/' . $file_name, $file_name);
         
         //Add your file name
-        $zip->setEncryptionName($file_name, ZipArchive::EM_AES_256, $pass); //Add file name and password dynamically
+        $result = $zip->setEncryptionName($file_name, ZipArchive::EM_AES_128, $pass); //Add file name and password dynamically
         $zip->close();
 
         //generate link
         $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
         $actual_link = str_replace("download.php", "downloads/" . $file_name . ".zip", $actual_link);
+
+        unlink("downloads/" . $file_name);
         
-        echo $actual_link;
+        echo "<a href=\"". $actual_link . "\">" .  $actual_link . "</a>";
     } else {
         echo '<p>failed to zip file</p>';
     }
