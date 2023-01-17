558da981366fec84327edf38baa4428947015f07
Andy Dunkel <a.dunkel@pilz.de>
2023-01-17 13:38:31 +0100

    Downloader, initial commit

---
+++ download.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+    <title>Download</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+<h1>The download</h1>
+
+<?php
+$url = "";
+$pass = "";
+
+if (isset($_POST["filename"])) {
+    $url = $_POST["filename"];
+} else {
+    die("<p>No file specified</p>");
+}
+
+if (isset($_POST["pass"])) {
+    $pass = $_POST["pass"];
+} else {
+    $pass = "1234";
+}
+
+$file_name = basename($url);
+    
+if (file_put_contents("downloads\\" . $file_name, file_get_contents($url)))
+{
+    echo "File downloaded successfully:";
+
+    $zip = new ZipArchive;
+    $res = $zip->open("downloads\\" . $file_name . ".zip", ZipArchive::CREATE); 
+    
+    //Add your file name
+    if ($res === TRUE) {
+        $zip->addFile('downloads\\' . $file_name, $file_name);
+        
+        //Add your file name
+        $zip->setEncryptionName($file_name, ZipArchive::EM_AES_256, $pass); //Add file name and password dynamically
+        $zip->close();
+
+        //generate link
+        $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $actual_link = str_replace("download.php", "downloads/" . $file_name . ".zip", $actual_link);
+        
+        echo $actual_link;
+    } else {
+        echo '<p>failed to zip file</p>';
+    }
+}
+else
+{
+    echo "<p>File downloading failed.</p>";
+}
+?>
+
+</body>
+</html>
